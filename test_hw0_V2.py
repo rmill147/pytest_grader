0e67d2d23822447e2e01b979b91c4253413f1339
Arthur Choi <achoi13@kennesaw.edu>
2024-03-03 16:12:23 -0500

    smoothed things out

--- test_hw0_V2.py
+++ test_hw0_V2.py
@@ -3,35 +3,17 @@ import glob
 import pytest
 from importlib import import_module
 
-@pytest.fixture(scope="module", autouse=True)
-def v2_setup():
-    # Runs before tests:
-    print("\n set-up....")
-    print("\n Files in submissions:")
-    for filename in os.listdir('submissions'):
-        f = os.path.join('submissions', filename)
-        # checking if it is a file
-        if os.path.isfile(f):
-            print(f)
-            # module_name = os.path.splitext(filename)[0]
-            # selected_student = "submissions.%s" % module_name
-            # student = import_module(selected_student)
-            # return student
 
-    yield
-    # Runs after tests:
-    print("tear-down...")
+def create_test_class(module_id,module_name):
 
+    class TestClass:
+        module_id = module_id
+        module_name = module_name
+        module = import_module(module_name)
+        passed = 0
+        tested = 0
 
-def create_test_hw_0(student_id,file_name):
-
-    class test_hw0:
-        def setup(self):
-            print(f"Student ID: {student_id}")
-            print(f"File name: {file_name}")
-
-        def test_output0(v2_setup):
-            student = import_module(file_name)
+        def test_0(self):
             expected = [('Princess Bride, The (1987)', 4.232394366197183, 142),
                         ('Pulp Fiction (1994)', 4.197068403908795, 307),
                         ("Amelie (Fabuleux destin d'Amélie Poulain, Le) (2001)", 4.183333333333334, 120),
@@ -40,26 +22,20 @@ def create_test_hw_0(student_id,file_name):
                         ('Fargo (1996)', 4.116022099447513, 181), ('Trainspotting (1996)', 4.03921568627451, 102),
                         ('Back to the Future (1985)', 4.038011695906433, 171),
                         ('Finding Nemo (2003)', 3.9609929078014185, 141), ('Groundhog Day (1993)', 3.944055944055944, 143)]
-            # student = picked_student()
-            actual = student.part0
+            actual = self.module.part0
             assert actual == expected
 
-        def test_output1(v2_setup):
-            student = import_module(file_name)
+        def test_1(self):
             expected = [('Usual Suspects, The (1995)', 4.237745098039215, 204)]
-            # student = picked_student()
-            actual = student.part1
+            actual = self.module.part1
             assert actual == expected
 
-        def test_output2(v2_setup):
-            student = import_module(file_name)
+        def test_2(self):
             expected = [('Waterworld (1995)', 2.9130434782608696, 115)]
-            # student = picked_student()
-            actual = student.part2
+            actual = self.module.part2
             assert actual == expected
 
-        def test_output3(v2_setup):
-            student = import_module(file_name)
+        def test_3(self):
             expected = [('Star Wars: Episode IV - A New Hope (1977)', 5.0, 104),
                         ('Star Wars: Episode V - The Empire Strikes Back (1980)', 4.8076923076923075, 78),
                         ('Star Wars: Episode VI - Return of the Jedi (1983)', 4.730263157894737, 76),
@@ -70,24 +46,18 @@ def create_test_hw_0(student_id,file_name):
                         22), ('In Bruges (2008)', 4.541666666666667, 12), (
                         'Raiders of the Lost Ark (Indiana Jones and the Raiders of the Lost Ark) (1981)', 4.536764705882353,
                         68)]
-            # student = picked_student()
-            actual = student.part3
+            actual = self.module.part3
             assert actual == expected
 
-    return test_hw0
+    return TestClass
 
-
-grades = {}
-i = 0
-for filename in os.listdir('submissions'):
-    print("filename:")
-    print(filename)
-    directory = 'submissions'
-    f = os.path.join('submissions', filename)
-    if os.path.isfile(f):
-        module_name = os.path.splitext(filename)[0]
-        selected_student = "%s.%s" % (directory,module_name)
-        classIteration = "Test" + str(i)
-        print(classIteration)
-        globals()[classIteration] = create_test_hw_0(i,selected_student)
-        i = i+1
+submission_dir = 'submissions'
+filenames = glob.glob("%s/*.py" % submission_dir)
+for module_id,filename in enumerate(filenames):
+    basename = os.path.basename(filename)
+    basename = os.path.splitext(basename)[0]
+    module_name = "%s.%s" % (submission_dir,basename)
+    class_name = "Test%d" % module_id
+    test_class = create_test_class(module_id,module_name)
+    test_class.basename = basename
+    globals()[class_name] = test_class
